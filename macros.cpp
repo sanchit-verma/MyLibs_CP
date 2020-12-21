c28410997c9ce6c639018b02c657833f7579a654
Sanchit Verma <46937120+sanchit-verma@users.noreply.github.com>
2020-12-21 20:35:35 +0530

    Create macros.cpp

---
+++ macros.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define endl "\n"
+#define endly " "
+#define int long long
+#define fr(i, a, b) for (ll i = a; a < b ? i <= b : i >= b; a < b ? i++ : i--)
+#define sz(x) ((int)x.size())
+#define len(x) ((int)x.length())
+#define F first
+#define S second
+#define all(x) begin(x), end(x)
+
+int32_t main()
+{
+      ios::sync_with_stdio(false);
+      cin.tie(0); 
+      return 0;
+}
